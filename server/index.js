34535fd578b9afd0fc48c7eb27fe784681e47ed4
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-05-13 13:58:44 +0200

    connecting to postgres through sequelize

--- server/index.js
+++ server/index.js
@@ -1,18 +1,4 @@
 import { ApolloServer, gql }  from 'apollo-server'
-import { Pool, Client } from 'pg'
-
-const connectionString = `postgressql://${process.env.dbUser}:${process.env.dbPassword}@localhost:5432/chat_with_me`
-
-const client = new Client({
-  connectionString
-})
-
-client.connect()
-
-client.query('SELECT * FROM users', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
 
 const types = fileLoader(path.join(__dirname, './schema'));
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')))
@@ -22,6 +8,8 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 const PORT = 8080
 
-server.listen(PORT).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+models.sequelize.sync().then(function () {
+  server.listen(PORT).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  }); 
 });
