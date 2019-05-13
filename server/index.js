373f5c85bc1b07f7e1a3d702683c10bb85ca851c
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-05-13 13:16:59 +0200

    connecting to the postgresql database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 import { ApolloServer, gql }  from 'apollo-server'
+import { Pool, Client } from 'pg'
+
+const connectionString = `postgressql://${process.env.dbUser}:${process.env.dbPassword}@localhost:5432/chat_with_me`
+
+const client = new Client({
+  connectionString
+})
+
+client.connect()
+
+client.query('SELECT * FROM users', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
 
 const types = fileLoader(path.join(__dirname, './schema'));
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')))
