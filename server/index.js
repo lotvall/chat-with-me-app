1141a51de26b1d2f7bddf066c118c5776e12ff96
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-05-13 17:48:25 +0200

    get user from header

--- server/index.js
+++ server/index.js
@@ -3,21 +3,28 @@ import path from 'path';
 import dotenv from 'dotenv'
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import models from './models/index'
+import jwt from 'jsonwebtoken'
 
 dotenv.config()
 
-console.log(process.env.dbUsername)
+const SECRET = process.env.SECRET1
+const SECRET2= process.env.SECRET2
+
 const types = fileLoader(path.join(__dirname, './schema'));
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')))
 const typeDefs = mergeTypes(types, { all: true });
 
 const server = new ApolloServer({ typeDefs, resolvers,
   context: ({ req, connection }) => {
+
+    const token = req.headers.token || '';
+
+    const { user } = jwt.verify(token, SECRET)
     return { 
       models,
-      user: connection ? connection.context.user : req.user,
-      SECRET: process.env.SECRET1,
-      SECRET2: process.env.SECRET2,
+      user,
+      SECRET,
+      SECRET2,
 
     };
   }
