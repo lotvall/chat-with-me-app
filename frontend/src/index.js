0e138d8a43d2d5de12879ffe3de8be05937a5dc3
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-05-15 13:35:38 +0200

    set up simple apollo client app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import Routes from './Routes'
+//import Routes from './Routes'
 import { ApolloProvider } from "react-apollo";
 import 'semantic-ui-css/semantic.min.css'
-import client from './apollo'
+// import client from './apollo'
+
+import ApolloClient from "apollo-boost";
+import { createUploadLink } from 'apollo-upload-client'
+
+
+const httpLink = createUploadLink({
+  uri: 'http://localhost:8080/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+})
 
 const App = (
     <ApolloProvider client={client}>
-        <Routes />  
+        <h2>An apollo app</h2> 
     </ApolloProvider>
 )
 
